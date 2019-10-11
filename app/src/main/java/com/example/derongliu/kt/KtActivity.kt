b4a3612a9115203616591f9b9565497f198d1e36
Derong Liu <derong.liu@ihandysoft.com>
2019-10-11 17:06:49 +0800

    kt初探

--- app/src/main/java/com/example/derongliu/kt/KtActivity.kt
+++ app/src/main/java/com/example/derongliu/kt/KtActivity.kt
@@ -2,9 +2,14 @@ package com.example.derongliu.kt
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.derongliu.androidtest.R
+import kotlinx.android.synthetic.main.kt_item.view.*
 
 class KtActivity : AppCompatActivity() {
 
@@ -13,22 +18,47 @@ class KtActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kt)
         val recyclerView = findViewById<RecyclerView>(R.id.rv_kt)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = MyAdapter(initData())
+
+    }
+
+    fun initData(): ArrayList<String> {
+        val data: ArrayList<String> = arrayListOf()
+        data.add("main")
+        data.add("one")
+        data.add("two")
+        data.add("three")
+        data.add("four")
+        return data
     }
 
 
+    private class MyAdapter(val items : ArrayList<String>) : RecyclerView.Adapter<MyHolder>() {
 
-    private class MyAdapter:RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-
-        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyHolder {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return MyHolder(LayoutInflater.from(p0.context).inflate(R.layout.kt_item, p0, false))
         }
 
         override fun getItemCount(): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return items.count()
         }
 
-        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        override fun onBindViewHolder(p0: MyHolder, p1: Int) {
+            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            p0.tv.text = items[p1]
+
+        }
+    }
+
+
+    private class MyHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tv: TextView
+
+        init {
+            tv = view.kt_item_tv;
         }
     }
 }
